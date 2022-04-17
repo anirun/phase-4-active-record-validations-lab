63e91225f554da0461c32c3d296ad90df19b8745
Nurina Junus <nurina.junus@gmail.com>
2022-04-17 17:33:14 -0600

    commit tree 461dbc52abe80c26565167273d9b57b4bd3e6333

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+validates :title, presence: :true
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: ["Fiction", "Non-Fiction"]
+validate :title_must_include
+
+private
+
+TITLE_INCLUDES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+]
+
+def title_must_include
+    if TITLE_INCLUDES.none? { |clickbait| clickbait.match title }
+        errors.add(:title, "must include clickbait")
+    end
+end
 end
